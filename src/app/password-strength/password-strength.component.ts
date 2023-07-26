f8d19fcde3b8e4e672b98228e534c2254eaa5dc2
Roman Romanko <rroma5129@gmail.com>
2023-07-26 09:00:20 +0300

    A simple design was created, a password display button was added.

--- src/app/password-strength/password-strength.component.ts
+++ src/app/password-strength/password-strength.component.ts
@@ -6,17 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./password-strength.component.scss'],
 })
 export class PasswordStrengthComponent implements OnInit {
+  textIndicator: string = 'Empty';
   password: string = '';
   passwordStrength: number = 0;
+  visiblePassword: boolean = false;
   ngOnInit(): void {}
 
   checkPasswordStrength(): void {
     this.passwordStrength = 0;
     if (this.password.length === 0) {
       this.passwordStrength = 0;
+      this.textIndicator = 'Empty';
       return;
-    } else if (this.password.length < 8) {
+    } else if (this.password.length <= 8) {
       this.passwordStrength = -1;
+      this.textIndicator = 'Less then 8 symbols';
       return;
     }
     if (/^(?=.*[0-9])/.test(this.password)) {
@@ -25,8 +29,23 @@ export class PasswordStrengthComponent implements OnInit {
     if (/^(?=.*[a-zA-Z])/.test(this.password)) {
       this.passwordStrength++;
     }
-    if (/^(?=.*[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/@#\\])/.test(this.password)) {
+    if (/^(?=.*[!@#$%^&*(),.?":{}|<>+=_])/.test(this.password)) {
       this.passwordStrength++;
     }
+    switch (this.passwordStrength) {
+      case 1:
+        this.textIndicator = 'Poor';
+        break;
+      case 2:
+        this.textIndicator = 'Medium';
+        break;
+      case 3:
+        this.textIndicator = 'Strong';
+        break;
+    }
+  }
+  changeVisible(): void {
+    this.visiblePassword = !this.visiblePassword;
+    console.log(this.visiblePassword);
   }
 }
